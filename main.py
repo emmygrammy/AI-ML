a04adddc95d34f9f765865cabdff1646f9e81fba
emmy grammy <ojeabuo24@gmail.com>
2026-09-16 14:35:09 +0100

    python

--- main.py
+++ main.py
@@ -8,3 +8,23 @@ class person:
 
 p1 = person('John', 'Doe')
 p1.printname()
+
+
+class student(person):
+    pass
+
+
+x = student('Mike', 'Olsen')
+x.printname()
+
+
+class student(person):
+    def __init__(self, fname, lname, graduationyear):
+        super().__init__(fname,lname)
+        self.graduationyear = graduationyear
+
+    def welcome(self):
+        print("Welcome", self.fname, self.lname , "to the class of", self.graduationyear)
+
+x=student('Mike', 'Olsen', 2019)
+x.welcome()
